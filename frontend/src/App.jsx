c9c9d74fff488403208a4b332dc54ffa9533b29f
kumaradityaapril <kumaraditya.bvn@gmail.com>
2026-03-23 10:44:21 +0530

    Added create feature flag form with API integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,25 +3,89 @@ import API from "./api/api";
 
 function App() {
   const [flags, setFlags] = useState([]);
+  const [form, setForm] = useState({
+    name: "",
+    enabled: true,
+    environment: "",
+    rollout_percentage: 0,
+    rules: "",
+    kill_switch: false,
+  });
 
-  // Fetch flags
   const getFlags = async () => {
     try {
       const res = await API.get("/flags");
-      setFlags(res.data.data); // because of APIResponse format
+      setFlags(res.data.data);
     } catch (err) {
       console.error(err);
     }
   };
 
-  // Run on page load
   useEffect(() => {
     getFlags();
   }, []);
 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const createFlag = async () => {
+    try {
+      const payload = {
+        ...form,
+        rules: JSON.parse(form.rules || "{}"),
+        rollout_percentage: Number(form.rollout_percentage),
+        enabled: form.enabled === "true" || form.enabled === true,
+        kill_switch: form.kill_switch === "true" || form.kill_switch === true,
+      };
+
+      await API.post("/flags", payload);
+      alert("Flag created successfully!");
+      getFlags();
+    } catch (err) {
+      console.error(err);
+      alert("Error creating flag");
+    }
+  };
+
   return (
     <div>
       <h1>Feature Flag Dashboard</h1>
+
+      <h2>Create Feature Flag</h2>
+
+      <input name="name" placeholder="Name" onChange={handleChange} />
+      <br />
+      <input name="environment" placeholder="Environment" onChange={handleChange} />
+      <br />
+      <input
+        name="rollout_percentage"
+        placeholder="Rollout %"
+        type="number"
+        onChange={handleChange}
+      />
+      <br />
+      <input
+        name="rules"
+        placeholder='Rules JSON (e.g. {"users":["user123"]})'
+        onChange={handleChange}
+      />
+      <br />
+      <select name="enabled" onChange={handleChange}>
+        <option value="true">Enabled</option>
+        <option value="false">Disabled</option>
+      </select>
+      <br />
+      <select name="kill_switch" onChange={handleChange}>
+        <option value="false">Kill Switch OFF</option>
+        <option value="true">Kill Switch ON</option>
+      </select>
+      <br />
+      <br />
+      <button onClick={createFlag}>Create Flag</button>
+
+      <hr />
 
       <h2>All Flags</h2>
 
