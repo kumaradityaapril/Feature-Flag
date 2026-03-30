6cc3c853da8f9dc8ae2e61b6999fe6b4f4895c1e
kumaradityaapril <kumaraditya.bvn@gmail.com>
2026-03-30 09:56:57 +0530

    small update in backend

--- backend/services/flag_service.go
+++ backend/services/flag_service.go
@@ -11,11 +11,10 @@ import (
 	"time"
 )
 
-// Helper methods for Redis Cache
 func cacheFlag(flag models.FeatureFlag) {
 	data, err := json.Marshal(flag)
 	if err == nil {
-		// Cache expires after 5 minutes to ensure eventual consistency
+		
 		config.RedisClient.Set(config.Ctx, "flag:"+flag.Name, data, 5*time.Minute)
 	}
 }
@@ -64,15 +63,15 @@ func UpdateFeatureFlag(id int, flag models.FeatureFlag) error {
 
 func EvaluateFlag(flagName string, req models.EvaluationRequest) (bool, error) {
 
-	// Check Redis cache first
+	
 	val, err := config.RedisClient.Get(config.Ctx, "flag:"+req.FlagName).Result()
 	var flag models.FeatureFlag
 	
 	if err == nil {
-		// Cache hit
+		
 		json.Unmarshal([]byte(val), &flag)
 	} else {
-		// Cache miss
+		
 		flag, err = repository.GetFlagByName(flagName)
 		if err != nil {
 			return false, err
@@ -82,32 +81,32 @@ func EvaluateFlag(flagName string, req models.EvaluationRequest) (bool, error) {
 
 	log.Println("Evaluating flag:", flag.Name)
 
-	// Kill Switch
+	
 	if flag.KillSwitch {
 		log.Println("Kill switch enabled")
 		return false, nil
 	}
 
-	// Environment Check
+	
 	if normalizeEnv(flag.Environment) != normalizeEnv(req.Environment) {
 		log.Println("Environment mismatch")
 		return false, nil
 	}
 
-	// Parse Rules
+	
 	rules := flag.Rules
 
-	// User Targeting (Whitelist)
+	
 	if users, ok := rules["users"].([]interface{}); ok && len(users) > 0 {
 		for _, u := range users {
 			if str, ok := u.(string); ok && str == req.UserID {
 				log.Println("User in whitelist - ENABLED")
-				return true, nil // Bypass the rest!
+				return true, nil 
 			}
 		}
 	}
 
-	// Country Targeting (Restrictive Filter)
+	
 	if countries, ok := rules["countries"].([]interface{}); ok && len(countries) > 0 {
 		found := false
 		for _, c := range countries {
@@ -122,7 +121,7 @@ func EvaluateFlag(flagName string, req models.EvaluationRequest) (bool, error) {
 		}
 	}
 
-	// Version Targeting (Restrictive Filter)
+	
 	if minVersion, ok := rules["min_version"].(string); ok && minVersion != "" {
 		if req.AppVersion != "" && req.AppVersion < minVersion {
 			log.Println("App version too low - DISABLED")
@@ -130,7 +129,7 @@ func EvaluateFlag(flagName string, req models.EvaluationRequest) (bool, error) {
 		}
 	}
 
-	// Percentage Rollout
+	
 	if flag.RolloutPercentage > 0 && flag.RolloutPercentage < 100 {
 		userPercent := getUserPercentage(req.UserID)
 		if userPercent >= flag.RolloutPercentage {
@@ -144,7 +143,7 @@ func EvaluateFlag(flagName string, req models.EvaluationRequest) (bool, error) {
 		return true, nil
 	}
 
-	// Global Boolean (Fallback)
+	
 	if !flag.Enabled {
 		log.Println("Flag disabled globally")
 		return false, nil
