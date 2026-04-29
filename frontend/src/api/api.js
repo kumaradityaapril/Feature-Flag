92747aafeef88351205cbc0cf088bafe97c8c3da
kumaradityaapril <kumaraditya.bvn@gmail.com>
2026-04-29 18:06:07 +0530

    Fixing cors issue

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8081",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://feature-flag-40we.onrender.com",
 });
 
 export default API;
