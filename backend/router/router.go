e54adf25d36af9118063165ae67e5447ae63cf5b
kumaradityaapril <kumaraditya.bvn@gmail.com>
2026-04-29 18:02:05 +0530

    Testing

--- backend/router/router.go
+++ backend/router/router.go
@@ -28,15 +28,10 @@ func SetupRouter() *gin.Engine {
 	}
 
 	r.Use(func(c *gin.Context) {
-		origin := c.Request.Header.Get("Origin")
-		if origin != "" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-		} else {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		}
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+		c.Writer.Header().Set("X-CORS-Status", "Permissive")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
